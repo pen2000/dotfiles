cb60c42ff95e365a7006f1b6143e3ca39d990073
Takumi Ono <ohno.pengvc@icloud.com>
2026-04-14 22:15:13 +0900

    ファイル名検索のキーマップ追加

--- nvim_lazyvim/lua/config/keymaps.lua
+++ nvim_lazyvim/lua/config/keymaps.lua
@@ -1,3 +1,12 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- ターミナルモード脱出
+vim.keymap.set("t", "<C-q>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
+
+-- カーソル下の単語でファイル名検索
+vim.keymap.set("n", "<leader>fw", function()
+  local word = vim.fn.expand("<cword>")
+  Snacks.picker.files({ pattern = word })
+end, { desc = "Find files (cursor word)" })
